1f3e3851b419757dcb3c6d16b6ce088e25ef4631
lyangdong <330338945@qq.com>
2017-07-23 03:47:34 +0800

    Update public.js

--- public.js
+++ public.js
@@ -1,10 +1,7 @@
-/**
- * Created by liu on 2016/7/23.
- */
 
 
 $(function(){
-    //Ä£ÄâÊı¾İ
+    //æ¨¡æ‹Ÿæ•°æ®
    var res = {
        "list":[
        {"money":"1110","time":"1"},
@@ -15,32 +12,32 @@ $(function(){
        ]
    };
     for(var i=0;i<5;i++){
-        //ÉèÖÃ½ÚµãºáÏò×ø±ê
+        //è®¾ç½®èŠ‚ç‚¹æ¨ªå‘åæ ‡
         var strLeft = i*$('.time').width() + $('.time').width()/2-$('.option').width()/2;
         $('.point').eq(i).css('left',strLeft);
 
-        //ÉèÖÃ½Úµã×İÏò×ø±ê
+        //è®¾ç½®èŠ‚ç‚¹çºµå‘åæ ‡
         var strBottom = ( Number(res.list[i].money) - 1100)*3/5 + 60 - 8;
         $('.point').eq(i).css('bottom',strBottom);
 
        if(i<=3){
-           //ÉèÖÃÁ¬½ÓÏß³¤¶È
+           //è®¾ç½®è¿æ¥çº¿é•¿åº¦
            var strBottom2 = ( Number(res.list[i + 1].money) - 1100) * 3 / 5 + 60 - 8;
            var dh = strBottom2 - strBottom;
            var tw = $('.time').width();
            var lh = Math.sqrt((tw * tw + dh * dh));
 
-           //ÉèÖÃÁ¬ÏßµÄ½Ç¶È
+           //è®¾ç½®è¿çº¿çš„è§’åº¦
            var A = Math.acos(tw / lh) / Math.PI * 180;
            $('.outline').eq(i).css('transform', 'rotate(' + (270 - A) + 'deg)');
 
-           //ÉèÖÃÁ¬ÏßÎ»×Ó
+           //è®¾ç½®è¿çº¿ä½å­
            $('.outline').eq(i).css('height', 2 * lh);
            $('.outline').eq(i).css('bottom', lh - 4);
            $('.line').eq(i).css('height', lh + 2);
        }
     }
-    //ÉèÖÃÊó±ê»¬¹ıĞ§¹û
+    //è®¾ç½®é¼ æ ‡æ»‘è¿‡æ•ˆæœ
     $('.trend').mouseenter(function(){
         $('.point').css('width',12);
         $('.point').css('height',12);
